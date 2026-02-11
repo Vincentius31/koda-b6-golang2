1ac673342fdbb234f1a01f9ae027d9d3c17720ac
Vincentius <vincentius85@gmail.com>
2026-02-11 15:15:34 +0700

    feat: adding new task to replace the number using slice partitioning

--- main.go
+++ main.go
@@ -2,137 +2,23 @@ package main
 
 import "fmt"
 
-//Nomor 1
-type the struct{
-	best string
-}
-
-type are struct{
-	the the
-}
-
-type weType struct{
-	are are
-}
-
-//Nomor 2
-type helloType struct{
-	world string
-}
-
-//Nomor 3
-type tech struct{
-	academy string
-}
-
-type man struct{
-	tech tech
-}
-
-type stritem struct{
-	man []man
-}
-
-type objtype struct{
-	str[][][]stritem
-}
-
-//Nomor 4
-type fruit struct{
-	is string
-}
-
-type favourite struct{
-	fruit fruit
-}
-
-type mytype struct{
-	favourite []favourite
-}
-
-//Nomor 5
-type numtype struct{
-	first []int
-	second []int
-}
-
 func main(){
-	//Hasil Nomor 1
-	var we weType = weType{
-		are: are{
-			the: the{
-				best: "Koda",
-			},
-		},
+	// var scores []int = []int{50, 75, 66, 20, 32,90,}
+	var scores []int = []int{66, 75, 50, 20, 32,90,}
+	// var scores []int = []int{90, 75, 50, 20, 32,66,}
+	var index int = 0
+	var target int = 66
+
+	for i, value := range scores{
+		if value == target {
+			index = i
+		}
 	}
 
-	fmt.Println(we.are.the.best)
-
-	//Hasil Nomor 2
-	var hello helloType = helloType{
-		world: "Hello World",
-	}
-
-	fmt.Println(hello.world)
-
-	//Hasil Nomor 3
-	var obj objtype = objtype{
-		[][][]stritem{
-			{},
-			{},
-			{},
-			{
-				{},
-				{
-					{},
-					{},
-					{
-						man: []man{
-							{
-								tech: tech{
-									academy: "Tech Academy",
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	fmt.Println(obj.str[3][1][2].man[0].tech.academy)
-
-	//Hasil Nomor 4
-	my := []mytype{
-		{
-			favourite: []favourite{
-				{},
-				{},
-				{},
-				{
-					fruit: fruit{
-						is: "Apple",
-					},
-				},
-			},
-		},
-	}
+	scores = append(scores[:index+1], append([]int {88,}, scores[index+1:]...)...)
 	
-	fmt.Println(my[0].favourite[3].fruit.is)
 
-	//Hasil Nomor 5
-
-	var num numtype = numtype{
-		first: []int{
-			10,
-			12,
-		},
-		second: []int{
-			5,
-			10,
-			20,
-		},
+	for i := 0; i < len(scores); i++ {
+		fmt.Println(scores[i])
 	}
-
-	fmt.Println(num.first[1] + num.second[2])
 }
